8597d8d4aaa904762f9ea84811368bff2bf60f02
efiVanso <efi.vanso@gmail.com>
2020-07-14 23:49:33 +0300

    add event listeners to the sqares

--- colorGame.js
+++ colorGame.js
@@ -10,6 +10,19 @@ var squares = document.querySelectorAll(".square");
 var pickedColor = colors[3];
 var colorDisplay = document.getElementById("colorDisplay");
 colorDisplay.textContent = pickedColor;
+
 for(var i=0; i< squares.length; i++){
     squares[i].style.backgroundColor = colors[i];
+
+    //add click listeners to squares
+    squares[i].addEventListener("click", function(){
+    var clickedColor = this.style.backgroundColor;
+    if (clickedColor === pickedColor){
+        alert("correct");
+    }
+    else{
+        alert("wrong");
+    }
+});
 }
+
