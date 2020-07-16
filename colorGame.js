06d4de12cbce00208f38b0e277c7f1c03027c72d
efiVanso <efi.vanso@gmail.com>
2020-07-16 12:05:29 +0300

    set hard mode

--- colorGame.js
+++ colorGame.js
@@ -1,4 +1,6 @@
-var colors = generateRandomColors(6);
+var numSquares = 6;
+
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
@@ -57,7 +59,7 @@ function randomColor(){
 
 //button reset event
 $("#reset").click(function() {
-    colors = generateRandomColors(6);
+    colors = generateRandomColors(numSquares);
     pickedColor = pickColor();
     colorDisplay.textContent = pickedColor;
         for (let i = 0; i < squares.length; i++) {
@@ -69,7 +71,8 @@ $("#reset").click(function() {
 $("#easyBtn").click(function() {
     $("#easyBtn").addClass("selected");
     $("#hardBtn").removeClass("selected");
-    colors = generateRandomColors(3);
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
     pickedColor = pickColor();
     colorDisplay.textContent = pickedColor;
     for (let i = 0; i < squares.length; i++) {
@@ -79,10 +82,19 @@ $("#easyBtn").click(function() {
             squares[i].style.display = "none";
         }
     }
+    h1.style.backgroundColor = "#232323";
 });
 
 $("#hardBtn").click(function() {
     $("#hardBtn").addClass("selected");
     $("#easyBtn").removeClass("selected");
-
+    numSquares =6;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+        squares[i].style.display = "block";
+    }
+    h1.style.backgroundColor = "#232323";
 });
