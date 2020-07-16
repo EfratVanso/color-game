acbea3d58382e02c6b95b3dd4017dba7aa9b781d
efiVanso <efi.vanso@gmail.com>
2020-07-16 15:15:48 +0300

    clraning up

--- colorGame.js
+++ colorGame.js
@@ -1,122 +1,112 @@
 var numSquares = 6;
+var colors = []; // generateRandomColors(numSquares);
+var pickedColor; //= pickColor();
 
-var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
-var pickedColor = pickColor();
-var colorDisplay = document.getElementById("colorDisplay");
-colorDisplay.textContent = pickedColor;
-
-var h1 = document.querySelector("h1")
+var h1 = document.querySelector("h1");
 var messageDisplay = document.querySelector("#message");
 var resetButton = document.querySelector("#reset");
+var modeButtons = document.querySelectorAll(".mode");
+var colorDisplay = document.getElementById("colorDisplay");
 
-for(var i=0; i< squares.length; i++){
-    squares[i].style.backgroundColor = colors[i];
+init();
 
-    //add click listeners to squares
-    squares[i].addEventListener("click", function(){
-    var clickedColor = this.style.backgroundColor;
-    if (clickedColor === pickedColor){
+function init() {
+  setUpModeButtons();
+  setUpSquares();
+  reset();
+}
+
+function setUpModeButtons() {
+  //mode buttons event listeners
+  for (let i = 0; i < modeButtons.length; i++) {
+    modeButtons[i].addEventListener("click", function () {
+      $(".mode").removeClass("selected");
+      this.classList.add("selected");
+
+      this.textContent === "Easy" ? (numSquares = 3) : (numSquares = 6);
+      reset();
+    });
+  }
+}
+
+function setUpSquares() {
+  //add click listeners to squares
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].addEventListener("click", function () {
+      var clickedColor = this.style.backgroundColor;
+
+      //if the user choses the right color
+      if (clickedColor === pickedColor) {
         messageDisplay.textContent = "Correct";
         changeColors(clickedColor);
         h1.style.backgroundColor = clickedColor;
         resetButton.textContent = "Play Again?";
-    }
-    else{
+     
+    } else {//wrong color
         this.style.backgroundColor = "#232323";
         messageDisplay.textContent = "Try Again";
-    }//if
-     blinkMessage();
+      } //if
+      blinkMessage();
+    });
+  } //for
+}
 
-});
-}//for 
-
-function changeColors(color){
-    for (let i = 0; i < squares.length; i++) {
-        squares[i].style.backgroundColor = color;        
+function reset() {
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.display = "block";
+    if (colors[i]) {
+      squares[i].style.backgroundColor = colors[i];
+    } else {
+      squares[i].style.display = "none";
     }
+  }
+  h1.style.backgroundColor = "steelblue";
+  $("#message").text("");
+  $("#reset").text("New Colors");
 }
 
-function pickColor(){
-    var random = Math.floor(Math.random() * colors.length);
-    return colors[random];
+function changeColors(color) {
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = color;
+  }
 }
 
-function generateRandomColors(num){
-    // add num random colors to array
-    var arr = [];
-    for (let i = 0; i < num; i++) {
-        arr[i] = randomColor();
-    }
-    return arr;
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
 }
 
-function randomColor(){
-    //pick green, red and blue between 0-255
-    var r = Math.floor(Math.random() * 256);
-    var g = Math.floor(Math.random() * 256);
-    var b = Math.floor(Math.random() * 256);
-    return "rgb("+r+", "+g+", "+b+")";
+function generateRandomColors(num) {
+  // add num random colors to array
+  var arr = [];
+  for (let i = 0; i < num; i++) {
+    arr[i] = randomColor();
+  }
+  return arr;
+}
+
+function randomColor() {
+  //pick green, red and blue between 0-255
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+  return "rgb(" + r + ", " + g + ", " + b + ")";
 }
 
 //button reset event
-$("#reset").click(function() {
-    colors = generateRandomColors(numSquares);
-    pickedColor = pickColor();
-    colorDisplay.textContent = pickedColor;
-        for (let i = 0; i < squares.length; i++) {
-            squares[i].style.backgroundColor = colors[i];        
-        }
-    h1.style.backgroundColor = "steelblue";
-    $("#message").text("");
-    $("#reset").text("New Colors");
-
+$("#reset").click(function () {
+  reset();
 });
 
-$("#easyBtn").click(function() {
-    $("#easyBtn").addClass("selected");
-    $("#hardBtn").removeClass("selected");
-    numSquares = 3;
-    colors = generateRandomColors(numSquares);
-    pickedColor = pickColor();
-    colorDisplay.textContent = pickedColor;
-    for (let i = 0; i < squares.length; i++) {
-        if(colors[i]){
-            squares[i].style.backgroundColor = colors[i];
-        }else{
-            squares[i].style.display = "none";
-        }
-    }
-    h1.style.backgroundColor = "steelblue";
-    $("#message").text("");
-});
-
-$("#hardBtn").click(function() {
-    $("#hardBtn").addClass("selected");
-    $("#easyBtn").removeClass("selected");
-    numSquares =6;
-    colors = generateRandomColors(numSquares);
-    pickedColor = pickColor();
-    colorDisplay.textContent = pickedColor;
-    for (let i = 0; i < squares.length; i++) {
-        squares[i].style.backgroundColor = colors[i];
-        squares[i].style.display = "block";
-    }
-    h1.style.backgroundColor = "steelblue";
-    $("#message").text("");
-});
-
-function blinkMessage()  {
-    $("#message").css("color", "red");
-    $("#message").css("font-size","22px");
-    setInterval(function(){ 
-        $("#message").css("color", "black");
-        $("#message").css("font-size","16px");
-     }, 300);
-
-    // $("#message").addClass("blink");
-    // setInterval(function(){ 
-    //     $("#message").removeClass("blink");
-    //  }, 600);
-
+function blinkMessage() {
+  $("#message").css("color", "red");
+  $("#message").css("font-size", "22px");
+  setInterval(function () {
+    $("#message").css("color", "black");
+    $("#message").css("font-size", "16px");
+  }, 300);
 }
